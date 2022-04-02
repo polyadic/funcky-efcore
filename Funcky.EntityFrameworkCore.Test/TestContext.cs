fb8ca22cdf37093b6002e19df4d4a03e424e5d37
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-04-02 11:18:00 +0200

    Use file scoped namespaces

--- Funcky.EntityFrameworkCore.Test/TestContext.cs
+++ Funcky.EntityFrameworkCore.Test/TestContext.cs
@@ -1,14 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Funcky.EntityFrameworkCore.Test
-{
-    internal sealed class TestContext : DbContext
-    {
-        public DbSet<Person> People { get; set; } = null!;
+namespace Funcky.EntityFrameworkCore.Test;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseInMemoryDatabase(databaseName: $"test-{Guid.NewGuid()}");
-        }
+internal sealed class TestContext : DbContext
+{
+    public DbSet<Person> People { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase(databaseName: $"test-{Guid.NewGuid()}");
     }
 }
--- Funcky.EntityFrameworkCore/Extensions/AsyncQueryableExtensions/LastOrNoneAsync.cs
+++ Funcky.EntityFrameworkCore/Extensions/AsyncQueryableExtensions/LastOrNoneAsync.cs
@@ -2,31 +2,30 @@ using System.Linq.Expressions;
 using Funcky.Monads;
 using Microsoft.EntityFrameworkCore;
 
-namespace Funcky.EntityFrameworkCore.Extensions
-{
-    public static partial class AsyncQueryableExtensions
-    {
-        /// <summary>
-        /// Returns the last element of a sequence as an <see cref="Option" />, or a <see cref="Option{T}.None" /> value if the sequence contains no elements.
-        /// </summary>
-        /// <typeparam name="TSource">the inner type of the queryable.</typeparam>
-        public static async Task<Option<TSource>> LastOrNoneAsync<TSource>(this IQueryable<TSource> source)
-            where TSource : notnull
-            => await source
-                .Select(x => Option.Some(x))
-                .LastOrDefaultAsync()
-                .ConfigureAwait(false);
+namespace Funcky.EntityFrameworkCore.Extensions;
 
-        /// <summary>
-        /// Returns the last element of a sequence that satisfies a condition as an <see cref="Option{T}" />  or a <see cref="Option{T}.None" /> value if no such element is found.
-        /// </summary>
-        /// <typeparam name="TSource">the inner type of the queryable.</typeparam>
-        public static async Task<Option<TSource>> LastOrNoneAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
-            where TSource : notnull
-            => await source
-                .Where(predicate)
-                .Select(x => Option.Some(x))
-                .LastOrDefaultAsync()
-                .ConfigureAwait(false);
-    }
+public static partial class AsyncQueryableExtensions
+{
+    /// <summary>
+    /// Returns the last element of a sequence as an <see cref="Option" />, or a <see cref="Option{T}.None" /> value if the sequence contains no elements.
+    /// </summary>
+    /// <typeparam name="TSource">the inner type of the queryable.</typeparam>
+    public static async Task<Option<TSource>> LastOrNoneAsync<TSource>(this IQueryable<TSource> source)
+        where TSource : notnull
+        => await source
+            .Select(x => Option.Some(x))
+            .LastOrDefaultAsync()
+            .ConfigureAwait(false);
+
+    /// <summary>
+    /// Returns the last element of a sequence that satisfies a condition as an <see cref="Option{T}" />  or a <see cref="Option{T}.None" /> value if no such element is found.
+    /// </summary>
+    /// <typeparam name="TSource">the inner type of the queryable.</typeparam>
+    public static async Task<Option<TSource>> LastOrNoneAsync<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+        where TSource : notnull
+        => await source
+            .Where(predicate)
+            .Select(x => Option.Some(x))
+            .LastOrDefaultAsync()
+            .ConfigureAwait(false);
 }
